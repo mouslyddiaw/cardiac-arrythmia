c68d52df70950bae28f25c56e622209c6326575f
Mously Diaw <mouslymatou@hotmail.com>
2020-11-05 10:33:53 +0000

    get categ/num variables

--- main.R
+++ main.R
@@ -5,6 +5,17 @@ library(readr)
 #Import data processed in jupyter notebook
 arrhythmia <- read_delim("Desktop/cardiac_arrythmia/data_arrhythmia_preprocess.csv", ";")
 
+#Get categorical and numerical variables
+categorical_cols <- c()
+for(col in names(arrhythmia )){
+  if (length(unique(arrhythmia[[col]])) == 2){
+     categorical_cols <- append(categorical_cols,col) 
+     #change type in r  
+  }
+}
 
+numerical_cols <- setdiff(names(arrhythmia ),categorical_cols)
+
+#Correlated numerical variables
 
 
