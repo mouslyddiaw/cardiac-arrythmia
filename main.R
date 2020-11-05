0c0d8f78f8d24f4dd76583bf3c0f7cc6b2a79466
Mously Diaw <mouslymatou@hotmail.com>
2020-11-05 13:23:04 +0000

    correlated features vf

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 ### Cardiac arrhythmia classification
 rm(list=ls()) 
 library(readr)
+library(GoodmanKruskal)
 
 #Import data processed in jupyter notebook
 arrhythmia <- read_delim("Desktop/cardiac_arrythmia/data_arrhythmia_preprocess.csv", ";")
@@ -16,21 +17,26 @@ for(col in names(arrhythmia )){
 
 numerical_cols <- setdiff(names(arrhythmia ),categorical_cols)
 
-#Correlated numerical variables
+#Drop highly correlated numerical variables
 corrmat<- cor(arrhythmia[,numerical_cols])
 
-highcorr <- data.frame()
+highcorrnum <- data.frame()
 
 for (i in seq(nrow(corrmat))){
   for (j in seq(ncol(corrmat))){
     if (corrmat[i,j]>0.9 & i!=j){
-      highcorr <- rbind(highcorr, c(rownames(corrmat)[i],colnames(corrmat)[j]))
+      highcorrnum <- rbind(highcorrnum, c(rownames(corrmat)[i],colnames(corrmat)[j]))
     }
   }
 }
 
-highcorr = highcorr[!duplicated(t(apply(highcorr, 1, sort))), ] #Removing duplicates
+highcorrnum <- highcorrnum[!duplicated(t(apply(highcorrnum, 1, sort))), ] #Removing duplicates
+#df = arrhythmia[,c("GO","II")]
+#m = as.matrix(df)
+#corPlot(m, method = "spearman")
 
-df = arrhythmia[,c("R'_wave","GG")]
-m = as.matrix(df)
-corPlot(m, method = "spearman")
+arrhythmia  <- subset(arrhythmia, select = -c(II,IO) )
+numerical_cols <- setdiff(numerical_cols,c("II","IO"))
+
+#Dimension reduction
+
