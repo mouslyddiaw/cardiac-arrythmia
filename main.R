5b56d963a4bafea15efc7dc663db2ab32b64b9f0
Mously Diaw <mouslymatou@hotmail.com>
2020-11-05 15:39:16 +0000

    boruta vf

--- main.R
+++ main.R
@@ -1,18 +1,19 @@
 ### Cardiac arrhythmia classification
 rm(list=ls()) 
 library("easypackages")  
-libraries("readr","party","leaps","caret")
+libraries("readr","party","leaps","caret","Boruta")
 
 
 #Import data processed in jupyter notebook
 arrhythmia <- read_delim("Desktop/cardiac_arrythmia/data_arrhythmia_preprocess.csv", ";")
+
 
 #Get categorical and numerical variables
 categorical_cols <- c()
 for(col in names(arrhythmia )){
   if (length(unique(arrhythmia[[col]])) == 2){
      categorical_cols <- append(categorical_cols,col) 
-     #change type in r  
+     arrhythmia[[col]] <- as.factor(arrhythmia[[col]])  
   }
 }
 
@@ -41,25 +42,28 @@ numerical_cols <- setdiff(numerical_cols,c("II","IO"))
 
 ###############################################
 #Feature selection 
+set.seed(111)
+boruta.arrhythmia <- TentativeRoughFix(Boruta(diagnosis~., data =  arrhythmia, doTrace = 2)) 
 
+getSelectedAttributes(boruta.arrhythmia, withTentative = F)
+
+#lda, lasso, random forest
+###############################################
+#Dimension reduction
 # Set seed for reproducibility
 set.seed(123)
 # Set up repeated k-fold cross-validation
 train.control <- trainControl(method = "cv", number = 10)
 # Train the model
 step.model <- train(diagnosis ~., data = arrhythmia,
-                    method = "leapBackward", 
-                    tuneGrid = data.frame(nvmax = 1:24),
+                    method = "pls",
+                    preProc = c("center", "scale"), 
+                    tuneLength = 15,
                     trControl = train.control
 ) 
+step.model$bestTune
 
-
-models <- regsubsets(diagnosis~., data = arrhythmia, nvmax = step.model$bestTune,method = "seqrep")
-summary(models)
-
-###############################################
-#Dimension reduction
-
+summary(step.model$finalModel)
 
 #cf1 <- cforest(diagnosis ~ . , data= arrhythmia, control=cforest_unbiased(mtry=2,ntree=50)) # fit the random forest
 #varimp(cf1)
